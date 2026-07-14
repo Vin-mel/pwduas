5998fa43b094d0acdeb23bfbc5b2049d7d772158
Yoas Christian Insu <yoaschristianinsu@itbss.ac.id>
2026-07-14 23:32:55 +0700

    Updet tipis tipis H-1 Tombol Edit admin

--- admin/renungan/hapus.php
+++ admin/renungan/hapus.php
@@ -22,7 +22,13 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
         mysqli_stmt_bind_param($stmt2, "i", $id);
         mysqli_stmt_execute($stmt2);
         mysqli_stmt_close($stmt2);
+        
+        $check = mysqli_query($conn, "SELECT COUNT(*) as total FROM tb_doa");
+        $row = mysqli_fetch_assoc($check);
 
+       if ($row['total'] == 0) {
+        mysqli_query($conn, "ALTER TABLE tb_doa AUTO_INCREMENT = 1");
+        }
         header("location: index.php?status=sukses");
         exit;
     } else {
